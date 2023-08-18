419827b99b23ed0c1bb24ea2c7e6171dbbf1e1e9
Christopher Battista <cattista@gmail.com>
2023-08-18 18:29:04 -0400

    Updates to structure of site.

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,41 @@
+// Modified Date in Title
 function modifiedTitle(pageName = document.title) {
     lastUpdate = new Date (document.lastModified);
     updateMonth = (lastUpdate.getMonth() + 1).toString();
     updateDay = lastUpdate.getDate().toString();
     updateYear = lastUpdate.getYear().toString().substr(-2);
 
-    document.title = pageName + " | CPC NEW HAVEN | as of " + updateMonth + "." + updateDay + "." + updateYear;
+    if (pageName){
+        pageName += " | "
+    }
+
+    document.title = pageName + "CPC NEW HAVEN | as of " + updateMonth + "." + updateDay + "." + updateYear;
 }
+
+
+// Footer Construction
+const footerTemplate = document.createElement('footer');
+
+footerTemplate.innerHTML = `
+<footer class="full-footer">
+    <!-- Bottom Footer Controls -->
+    <div id="footer-controls" class="buttons">
+    <a href="live.html" class="button">Live Stream</a>
+    <a href="latest-podcast.html" class="button">Latest Podcast</a>
+    <a href="weekly.html" class="button">Weekly Announcements</a>
+    <a href="lifegroups.html" class="button">Lifegroups</a>
+    <a href="archives.html" class="button">CPC Archives</a>
+    <a href="yearbook.html" class="button">Yearbook</a>
+    </div>
+    <div class="footer-inner-wrap">
+    <p>
+        <a href="tel:2037776960">203.777.6960</a> | admin@cpcnewhaven.org | 135 Whitney Ave. New Haven CT 06510
+        <br>
+        &copy; 2023 Christ Presbyterian Church.
+    </p>
+    </div>
+</footer>
+`;
+
+window.alert(footerTemplate.innerHTML);
+document.body.appendChild(footerTemplate.innerHTML);
